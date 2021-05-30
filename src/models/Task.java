10598f522f4a3a2eceb096851fe8dfdf22e5fc80
yuya.akabane <yuya.akabane.001@gmail.com>
2021-05-30 14:35:24 +0900

    Hotfix database files

--- src/models/Task.java
+++ src/models/Task.java
@@ -29,9 +29,6 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
-
     @Column(name = "content", length = 255, nullable = false)
     private String content;
 
@@ -47,14 +44,6 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
